9df81c7c4fc64ceb1d2df0f65d3310efa3fd1655
hillock liu <liu.lig@gmail.com>
2019-01-08 17:41:03 +0800

    修改入口参数可选

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ use Slim\Http\Response;
 
 $app->get('/home','\Controllers\Admin\HomeController:home');
 $app->get('/users/[{id}]','\Controllers\Users\PhotosController:index');
-$app->get('/wall/{id}','\Controllers\Users\PhotosController:wall');
+$app->get('/wall/[{id}]','\Controllers\Users\PhotosController:wall');
 $app->get('/','\Controllers\Users\PhotosController:wall');
 $app->get('/weixin','\Controllers\weixin\IndexController:vaild');
 $app->get('/ttt','\Controllers\weixin\IndexController:ttt');
